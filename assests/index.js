6de47004c610b4b130a603c42de83d519955fe6a
khalid abubaker <khalid7.bkr@gmail.com>
2020-06-13 13:16:29 +0300

    added ajax validation to contact me form

--- assests/index.js
+++ assests/index.js
@@ -16,6 +16,35 @@ $( document ).ready(function() {
       var status = $('#status').val();
 
 
+      function validateForm() {
+        if (name == "") {
+            document.getElementById("status").innerHTML = "Name cannot be empty";
+            return false;
+        }
+        if (email == "") {
+            document.getElementById("status").innerHTML = "Email cannot be empty";
+            return false;
+        } else {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if(!re.test(email)){
+                document.getElementById("status").innerHTML = "Email format invalid";
+                return false;
+            }
+        }
+        if (subject == "") {
+            document.getElementById("status").innerHTML = "Subject cannot be empty";
+            return false;
+        }
+        if (message == "") {
+            document.getElementById("status").innerHTML = "Message cannot be empty";
+            return false;
+        }
+        document.getElementById("status").innerHTML = "Sending...";
+        return true;
+      }
+
+      if(!validateForm())  return false;
+
       //send to formspree
       $.ajax({
         url:"https://formspree.io/mknqbqbb",
@@ -37,7 +66,11 @@ $( document ).ready(function() {
 
       });
 
+
+
     });
+
+
 
 
 

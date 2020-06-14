ae6413a53ba1162b194e4008cae38ce31f58ea44
khalid abubaker <khalid7.bkr@gmail.com>
2020-06-15 02:48:45 +0300

    added projects section, fixed minor bugs, code clean up

--- assests/index.js
+++ assests/index.js
@@ -1,6 +1,9 @@
 $( document ).ready(function() {
   new WOW().init();
 
+
+  $('.overlay').hover(function(){ $(this).toggleClass('animated pulse') }); //overlay on project card animation.
+
   $('#my-form').on('submit', function(e) {
       e.preventDefault();
 
